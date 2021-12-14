6afb2437e83847ff0f2e5031859af8b4731ae9b4
XuanxiangHuang <xuanxiang.huang.cs@outlook.com>
2021-12-14 01:23:11 +0100

    remove checking function

--- run.py
+++ run.py
@@ -12,76 +12,6 @@ from xddnnf.xpsdd import XpSdd
 ################################################################################
 
 
-def checkMHS(in_axps: list, in_cxps: list):
-    # given a list of axp and a list of cxp,
-    # check if they are minimal-hitting-set (MHS) of each other
-    # 1. uniqueness, and no subset(superset) exists;
-    if not in_axps or not in_cxps:
-        print(f"input empty: {in_axps}, {in_cxps}")
-        return False
-    axps = sorted(in_axps, key=lambda x: len(x))
-    axps_ = axps[:]
-    while axps:
-        axp = axps.pop()
-        set_axp = set(axp)
-        for ele in axps:
-            set_ele = set(ele)
-            if set_axp.issuperset(set_ele) or set_axp.issubset(set_ele):
-                print(f"axp is not unique: {set_axp}, {set_ele}")
-                return False
-    cxps = sorted(in_cxps, key=lambda x: len(x))
-    cxps_ = cxps[:]
-    while cxps:
-        cxp = cxps.pop()
-        set_cxp = set(cxp)
-        for ele in cxps:
-            set_ele = set(ele)
-            if set_cxp.issuperset(set_ele) or set_cxp.issubset(set_ele):
-                print(f"cxp is not unique: {set_cxp}, {set_ele}")
-                return False
-    # 2. minimal hitting set;
-    for axp in axps_:
-        set_axp = set(axp)
-        for cxp in cxps_:
-            set_cxp = set(cxp)
-            if not (set_axp & set_cxp):  # not a hitting set
-                print(f"not a hitting set: axp:{set_axp}, cxp:{set_cxp}")
-                return False
-    # axp is a MHS of cxps
-    for axp in axps_:
-        set_axp = set(axp)
-        for ele in set_axp:
-            tmp = set_axp - {ele}
-            size = len(cxps_)
-            for cxp in cxps_:
-                set_cxp = set(cxp)
-                if tmp & set_cxp:
-                    size -= 1
-            if size == 0:  # not minimal
-                print(f"axp is not minimal hitting set: "
-                      f"axp {set_axp} covers #{len(cxps_)}, "
-                      f"its subset {tmp} covers #{len(cxps_) - size}, "
-                      f"so {ele} is redundant")
-                return False
-    # cxp is a MHS of axps
-    for cxp in cxps_:
-        set_cxp = set(cxp)
-        for ele in set_cxp:
-            tmp = set_cxp - {ele}
-            size = len(axps_)
-            for axp in axps_:
-                set_axp = set(axp)
-                if tmp & set_axp:
-                    size -= 1
-            if size == 0:
-                print(f"cxp is not minimal hitting set: "
-                      f"cxp {set_cxp} covers #{len(axps_)}, "
-                      f"its subset {tmp} covers #{len(axps_) - size}, "
-                      f"so {ele} is redundant")
-                return False
-    return True
-
-
 def usage():
     """
         Prints usage message.
@@ -194,21 +124,12 @@ if __name__ == '__main__':
             if all_xp:
                 print("list all XPs ...")
                 axps, cxps = xpsdd.enum_exps()
-                # check each axp and cxp
-                for item in axps:
-                    assert xpsdd.check_one_axp(item)
-                for item in cxps:
-                    assert xpsdd.check_one_cxp(item)
-                # assert axps are MHS of cxps, vice versa
-                assert checkMHS(axps, cxps)
             elif xtype == 'axp':
                 print("find an axp ...")
                 axp = xpsdd.find_axp()
-                assert xpsdd.check_one_axp(axp)
             elif xtype == 'cxp':
                 print("find a cxp ...")
                 cxp = xpsdd.find_cxp()
-                assert xpsdd.check_one_cxp(cxp)
     else:
         print("haven't implemented")
         exit()
